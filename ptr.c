4e6491cb1bc7762a93ef78bb6289cb94c2b418f5
Sonal Dharangaonkar <SDharangaonkar12345@gmail.com>
2022-10-12 16:14:48 +0530

    POINTER

---
+++ ptr.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+    int a = 50;
+    int *p;
+    p = &a;
+
+    printf("\n Address of a : %u", &a);
+    printf("\n Address of p : %u", &p);
+
+    printf("\n Value of a : %d", a);
+    printf("\n Value of p : %d", p);
+
+    printf("\n Value of *p : %d", *p);
+
+    return 0;
+}
